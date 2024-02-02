bc470b7a6f81b48815e2896d471f55590c70d36e
dmitry <ddovidovich@list.ru>
2024-02-02 20:54:54 +0300

    first commit

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,33 @@
+type AddressType = {
+    street: string
+    city: string
+};
+
+export type UserType = {
+    id: number
+    name: string
+    age: number
+    address: AddressType;
+};
+
+export type UserListPropsType = {
+    users: UserType[]
+};
+
+export const UserList = (props: UserListPropsType) => {
+    return (
+        <div id={'hw01-users'}>
+            <h2>User List:</h2>
+
+            <ul>
+                {props.users.map((user) => {
+                    return (<li key={user.id} id={`hw01-user-${user.id}`}>
+                            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+                            {user.address.street}, {user.address.city}
+                        </li>)
+                })}
+
+            </ul>
+        </div>
+    );
+};
